c03385bb092f0037d1778eb55501e38246acd3e6
Frompaje <yanedwards11@gmail.com>
2025-01-04 17:41:55 -0300

    fix: env values

--- src/env/env.ts
+++ src/env/env.ts
@@ -3,12 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const env = {
-  HOSTNAME: process.env["HOSTNAME"] || "localhost",
-  PORT: process.env["PORT"] || "3000",
-  PG_USERNAME: process.env["PG_USERNAME"] || "postgres",
-  PG_PASSWORD: process.env["PG_PASSWORD"] || "password",
-  PG_DATABASE: process.env["PG_DATABASE"] || "default_pg",
-  DATABASE_URL:
-    process.env["DATABASE_URL"] ||
-    "postgresql://johndoe:randompassword@localhost:5432/mydb?schema=public",
+  HOSTNAME: process.env["HOSTNAME"],
+  PORT: process.env["PORT"],
+  PG_USERNAME: process.env["PG_USERNAME"],
+  PG_PASSWORD: process.env["PG_PASSWORD"],
+  PG_DATABASE: process.env["PG_DATABASE"],
+  DATABASE_URL: process.env["DATABASE_URL"],
 };
