ec00b3d142fd08bc9f380c17422a4d3a457b4c9a
Frompaje <yanedwards11@gmail.com>
2025-01-21 12:35:57 -0300

    feat: add CORS support to the Express application

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,18 @@ import express from "express";
 import { env } from "./infra/env/env";
 import { logger } from "./infra/logger";
 import { rootRouter } from "./infra/router";
+import cors from "cors";
+
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 rootRouter(app);
 
